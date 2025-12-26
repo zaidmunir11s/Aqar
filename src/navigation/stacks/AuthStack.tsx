9430f8f0f2e94709755b1018cb739cd038794999
Zaid Munir <zaidmunir11s@gmail.com>
2025-12-26 16:09:43 +0500

    Revert "Reapply "profile section""
    This reverts commit c4f3aea0cd4815a1d75412b7689301cca99fcdf8.

--- src/navigation/stacks/AuthStack.tsx
+++ src/navigation/stacks/AuthStack.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "../../screens/Auth/LoginScreen";
 import CreateAccountScreen from "../../screens/Auth/CreateAccountScreen";
-import ProfileDetailScreen from "../../screens/profile/ProfileDetailScreen";
-import UserProfileAdsScreen from "../../screens/profile/UserProfileAdsScreen";
-import PayBrokerCommissionScreen from "../../screens/profile/PayBrokerCommissionScreen";
 
 const Stack = createStackNavigator();
 
@@ -13,12 +10,6 @@ export default function AuthStack(): React.JSX.Element {
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="CreateAccount" component={CreateAccountScreen} />
-      <Stack.Screen name="ProfileDetail" component={ProfileDetailScreen} />
-      <Stack.Screen name="UserProfileAds" component={UserProfileAdsScreen} />
-      <Stack.Screen
-        name="PayBrokerCommission"
-        component={PayBrokerCommissionScreen}
-      />
     </Stack.Navigator>
   );
 }
